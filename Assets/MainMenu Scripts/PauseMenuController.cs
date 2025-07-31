78a53185256225f75c7efefd9db758c098e7e781
Bella P. <bellaperezppg@gmail.com>
2025-07-31 17:41:48 -0400

    Integrated Base Pause Menu
    Added some more assets and folder organization need to esc for pause menu lastly then added visual assets later on

---
+++ Assets/MainMenu Scripts/PauseMenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public string levelToLoad;
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturntoMainMenu()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+   
+}
